56ae4928da572f47c0880e6f79eeb6a0e5dbea12
Rafael Quintela <rafael4sq@gmail.com>
2020-03-10 17:32:01 -0300

    test

--- application.go
+++ application.go
@@ -18,7 +18,7 @@ func main() {
 	r := chi.NewRouter()
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome!!!")
+		fmt.Fprintf(w, "Chi")
 	})
 
 	log.Printf("Listening on port %s\n\n", port)
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/rafaelsq/drotes
 
 go 1.13
 
-require github.com/go-chi/chi v4.0.3+incompatible // indirect
+require github.com/go-chi/chi v4.0.3+incompatible
