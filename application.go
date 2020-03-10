9fc7cd6b3a5008191e180cc9e06a41d67729a92e
Rafael Quintela <rafael4sq@gmail.com>
2020-03-10 17:36:56 -0300

    test

--- application.go
+++ application.go
@@ -18,7 +18,7 @@ func main() {
 	r := chi.NewRouter()
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Chi")
+		fmt.Fprintf(w, "Welcome!")
 	})
 
 	log.Printf("Listening on port %s\n\n", port)
