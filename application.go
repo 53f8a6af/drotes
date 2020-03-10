c443198c2f64841272e94e608a9b2548cf98f21e
Rafael Quintela <rafael4sq@gmail.com>
2020-03-10 09:55:58 -0300

    owl

---
+++ application.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000"
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Drotes")
+	})
+
+	log.Printf("Listening on port %s\n\n", port)
+
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
