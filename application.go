05da7222464bab444beef191ec2502b2e13d27df
Rafael Quintela <rafael4sq@gmail.com>
2020-03-10 10:41:08 -0300

    welcome

--- application.go
+++ application.go
@@ -14,7 +14,7 @@ func main() {
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Drotes")
+		fmt.Fprintf(w, "Welcome!")
 	})
 
 	log.Printf("Listening on port %s\n\n", port)
